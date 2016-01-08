08f4023a7d3be2af90f45e755c33eec44c9e0cce
ryou62525 <rkc_tw_st@yahoo.co.jp>
2016-01-08 17:44:22 +0900

    更新

--- enemyTest/src/main.cpp
+++ enemyTest/src/main.cpp
@@ -1,15 +1,19 @@
 ﻿
 #include "app.h"
- 
+#include "object/enemy.h" 
+
 int main() {
   
 	App::Get();
 
   while (App::Get().isOpen()) {
+
+	  setEnemy();
+	  
 	  App::Get().begin();
 
 
-
+	  testDraw();
 
 
 
--- enemyTest/src/object/enemy.h
+++ enemyTest/src/object/enemy.h
@@ -13,7 +13,7 @@ struct Status
 enum class EnemyID
 {
 	ENEMY_01,
-	ENEMY_02, 
+	ENEMY_02,
 	ENEMY_03,
 	MAX_ENEMY
 };
@@ -21,10 +21,74 @@ enum class EnemyID
 class Enemy
 {
 
+
+protected:
+
 	Status state;
+	int hp;
 
 public:
 
-
-
+	void setpos(Vec2f newpos) { state.pos = newpos; }
+	virtual void draw() = 0;
 };
+
+class Slime : public Enemy
+{
+
+public:
+
+	Slime()
+	{
+		hp = 100;
+		state.size = Vec2f(32, 32);
+	}
+
+	
+	//void setpos(Vec2f newpos) { state.pos = newpos; }
+	void draw() { drawFillBox(state.pos.x(), state.pos.y(), state.size.x(), state.size.y(), Color::blue); }
+};
+
+class Dragon : public Enemy
+{
+
+
+public:
+
+	Dragon()
+	{
+		hp = 100;
+		state.size = Vec2f(32, 32);
+	}
+
+	void draw() { drawFillBox(state.pos.x(), state.pos.y(), state.size.x(), state.size.y(), Color::red); }
+};
+
+#define ENEMY_LIST 3
+
+
+Enemy *enemy[ENEMY_LIST] =
+{
+	new Slime,
+	new Slime,
+	new Dragon,
+};
+
+void setEnemy()
+{
+	Random rand;
+	rand.setSeed(time(nullptr));
+
+	for (int i = 0; i < ENEMY_LIST; ++i)
+	{
+		enemy[i]->setpos(Vec2f(rand(-100, 100), rand(-100, 100)));
+	}
+}
+
+void testDraw()
+{
+	for (int i = 0; i < ENEMY_LIST; ++i)
+	{
+		enemy[i]->draw();
+	}
+}
